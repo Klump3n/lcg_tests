6d158b4890863e4d1bfe6592a1e7f12d37bda981
Matthias Plock <plock@physik.hu-berlin.de>
2019-05-18 21:31:01 +0200

    parallelized autocorrelation test

--- modules/statistical_autocorrelation.py
+++ modules/statistical_autocorrelation.py
@@ -5,7 +5,23 @@ Perform autocorrelation tests.
 """
 import numpy as np
 
+import multiprocessing
+from itertools import repeat
+
 from util.logging.logger import CoreLog as cl
+
+def autocorrelate_tau(bin_seq, tau):
+    """
+    XOR a part of the given binary sequence with a shifted (by tau) part of the
+    same sequence. return the sum.
+
+    This is bitwise autocorrelation.
+    http://noosphere.princeton.edu/bitwise.autocorrelation.html
+
+    """
+    seq1 = bin_seq[0:5000]
+    seq2 = bin_seq[tau:tau+5000]
+    return np.sum(seq1 ^ seq2)
 
 def autocorrelation_passed(binary_sequence):
     """
@@ -14,12 +30,15 @@ def autocorrelation_passed(binary_sequence):
     """
     test_passed = True
 
+    b = np.asarray(list(binary_sequence), dtype=int)
+
     seqset = int(len(binary_sequence) / 4)
     tau = range(1,seqset)
-    v = np.asarray(list(binary_sequence[0:seqset]), dtype=int)
-    for t in tau:
-        vtau = np.asarray(list(binary_sequence[t:t+seqset]), dtype=int)
-        ctau = np.sum(v ^ vtau)  # XOR
+
+    with multiprocessing.Pool(4) as p:
+        res = p.starmap(autocorrelate_tau, zip(repeat(b), tau))
+
+    for t, ctau in enumerate(res):
         if not (2326 < ctau) or not (ctau < 2674):
             cl.verbose_warning("Autocorrelation failed for tau = {}, needs "
                                "2326 < {} < 2674".format(t, ctau))
